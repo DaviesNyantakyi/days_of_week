655bdc326dcbab593244b2a9c651df911ed4fd1f
DaviesNyantakyi <davies.nyantakyi@outlook.col>
2024-03-05 01:54:45 +0100

    daysOfWeek method added

---
+++ days_of_week/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+class MyApp
+{
+
+    class Program
+    {
+       static void Main()
+        {
+            Console.Write("Enter a number for the week: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            string day = getDaysOfWeek(num);
+            Console.WriteLine(day);
+            
+        }
+    
+        static string getDaysOfWeek (int num)
+        {
+            string dayNum;
+
+            switch (num)
+            {
+                case 1:
+                    dayNum = "Monday";
+                    break;
+                case 2:
+                    dayNum = "Teusday";
+                    break;
+                case 3:
+                    dayNum = "Wensday";
+                    break;
+                case 4:
+                    dayNum = "Thursday";
+                    break;
+                case 5:
+                    dayNum = "Friday";
+                    break;
+                default:
+                    dayNum = "invalid number";
+                    break;
+            }
+            return dayNum;
+        }
+    }
+}
